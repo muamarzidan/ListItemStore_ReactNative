ed0ffd6122b28ef2b105eafaebe4006a60ca0898
Muamar Zidan Tri Antoro <89593258+muamarzidan@users.noreply.github.com>
2023-06-07 02:46:53 +0700

    day1 add page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Button} from 'react-native'
 import React from 'react'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ECF8F9',
+    marginHorizontal: 20
+  },
+});
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
+    <View>  
+      <View style={styles.container}>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{color: 'black'}}>Halo, selamat datang di Toko Tata Jaya Mandiri</Text>
+          <Button
+            // onPress={onPressLearnMore}
+            title="Login"
+            color="#068DA9"
+          />
+          <Button
+            // onPress={onPressLearnMore}
+            title="Cari Barang"
+            color="#068DA9"
+          />
+        </View>
+      </View>
     </View>
   )
-}
+};
