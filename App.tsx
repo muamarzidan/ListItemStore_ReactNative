37daa9fd2505678e3f5e6ce6e385f0a10a288bbe
Muamar Zidan Tri Antoro <89593258+muamarzidan@users.noreply.github.com>
2023-06-07 13:58:41 +0700

    day1 build navigate

--- App.tsx
+++ App.tsx
@@ -1,44 +1,25 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: '#ECF8F9',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    paddingHorizontal: 20,
-    alignItems: 'center',
-  },
-  text: {
-    color: 'black',
-    textAlign: 'center',
-    marginBottom: 20,
-    fontSize: 20,
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#068DA9',
-    padding: 10,
-    width: 150,
-    borderRadius: 5,
-  }
-});
+import Index from './src/index';
+// import Login from './src/pages/LoginPage';
+// import Dashboard from './src/pages/DashboardPage';
+// import Item from './src/pages/ItemPage';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.main}>
-      <View style={styles.container}>
-        <Text style={styles.text}>Halo, selamat datang di Toko ...</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={{color: 'white'}}>Login</Text>
-       </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={{color: 'white'}}>Cari Daftar Barang</Text>
-       </TouchableOpacity>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Index"
+        screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Index" component={Index} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,49 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationProp } from '@react-navigation/native';
+import React from 'react';
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#ECF8F9',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+    textAlign: 'center',
+    marginBottom: 20,
+    fontSize: 20,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#068DA9',
+    padding: 10,
+    width: 150,
+    borderRadius: 5,
+  }
+});
+
+interface AppProps {
+  navigation: NavigationProp<any>;
+}
+
+export default function App({navigation}: AppProps) { 
+  return (
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Halo, selamat datang di Toko ...</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('loginpage')} style={styles.button}>
+          <Text style={{color: 'white'}}>Login</Text>
+       </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{color: 'white'}}>Cari Daftar Barang</Text>
+       </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
