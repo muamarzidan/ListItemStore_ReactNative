62468f3ea829bb039b40ae2cb86a6edbe30c0826
Muamar Zidan Tri Antoro <89593258+muamarzidan@users.noreply.github.com>
2023-06-15 15:18:22 +0700

    fix bug update EMOSI

--- src/pages/controllers/update.tsx
+++ src/pages/controllers/update.tsx
@@ -44,70 +44,68 @@ const UpdatePage = ({navigation}: {navigation: any}) => {
 
   return (
     <View style={styles.container}>
-  <Text style={styles.title}>Data Barang</Text>
-  <ScrollView contentContainerStyle={styles.contentContainer}>
-    {dataBarang.map((item, index) => (
-      <View style={styles.tableRow} key={`${item.kodeBarang}-${index}`}>
-        <Text style={styles.tableData}>{item.kodeBarang}</Text>
-        <Text style={styles.tableData}>{item.namaBarang}</Text>
-        <TouchableOpacity
-          style={styles.updateButton}
-          onPress={() =>
-            handleUpdateBarang(
-              item.kodeBarang,
-              item.namaBarang,
-              item.hargaAwal,
-              item.hargaJual,
-            )
-          }>
-          <Text style={styles.buttonText}>Update</Text>
-        </TouchableOpacity>
-      </View>
-    ))}
-  </ScrollView>
-</View>
-
+      <Text style={styles.title}>Data Barang</Text>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        {dataBarang.map((item, index) => (
+          <View style={styles.tableRow} key={`${item.kodeBarang}-${index}`}>
+            <Text style={styles.tableData}>{item.kodeBarang}</Text>
+            <Text style={styles.tableData}>{item.namaBarang}</Text>
+            <TouchableOpacity
+              style={styles.updateButton}
+              onPress={() =>
+                handleUpdateBarang(
+                  item.kodeBarang,
+                  item.namaBarang,
+                  item.hargaAwal,
+                  item.hargaJual,
+                )
+              }>
+              <Text style={styles.buttonText}>Update</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingHorizontal: 5,
-    },
-    contentContainer: {
-      flexGrow: 1,
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      textAlign: 'center',
-    },
-    tableRow: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginBottom: 10,
-    },
-    tableData: {
-      flex: 1,
-      fontSize: 16,
-      textAlign: 'center',
-    },
-    updateButton: {
-      backgroundColor: 'blue',
-      paddingVertical: 8,
-      paddingHorizontal: 12,
-      borderRadius: 5,
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 5,
+  },
+  contentContainer: {
+    flexGrow: 1,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  tableData: {
+    flex: 1,
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  updateButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default UpdatePage;
